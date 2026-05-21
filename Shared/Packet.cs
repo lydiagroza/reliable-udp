1f2188550d3a6fdd6ea437fe4e339490f3737ffc
lydiagroza <lydiagroza@gmail.com>
2026-05-21 20:51:13 +0300

    init + packet class

---
+++ Shared/Packet.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using System.Text.Json;
+
+namespace Shared;
+
+public enum PacketType
+{
+    SYN,
+    SYNACK,
+    ACK,
+    DATA
+}
+
+public class Packet
+{
+    public PacketType Type { get; set; }
+    public int SequenceNumber { get; set; }
+    public int WindowSize { get; set; }
+    public byte[] Data { get; set; } = Array.Empty<byte>();
+
+    public byte[] Serialize()
+    {
+        string json = JsonSerializer.Serialize(this);
+        return System.Text.Encoding.UTF8.GetBytes(json);
+    }
+
+    public static Packet Deserialize(byte[] data)
+    {
+        string json = System.Text.Encoding.UTF8.GetString(data);
+        return JsonSerializer.Deserialize<Packet>(json)!;
+    }
+}
