4a9cf37afb61e27a7f33a677cf90e0ae97491de3
lydiagroza <lydiagroza@gmail.com>
2026-05-22 00:09:17 +0300

    Client logic

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net;
+using System.Net.Quic;
 using System.Net.Sockets;
+using System.Threading.Tasks.Dataflow;
 using Shared;
 
 class Client
@@ -23,11 +25,33 @@ class Client
             WindowSize = desiredWindow 
         };
 
-    udpClient.Send(syn.Serialize(), serverEndPoint);
-    Console.WriteLine($"Sent SYN with window size: {desiredWindow}");
+        udpClient.Send(syn.Serialize(), serverEndPoint);
+        Console.WriteLine($"Sent SYN with window size: {desiredWindow}");
 
-    //
 
+        //step 2 -> receive SYN-ACK
+        IPEndPoint remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        byte[] synAckBytes = udpClient.Receive(ref remoteEP);
+        Packet synAck = Packet.Deserialize(synAckBytes);
+
+        if(synAck.Type != PacketType.SYNACK)
+            {
+                Console.WriteLine("Expected SYN-ACK, got something else.    Aborting");
+                return;
+            }
+
+        int agreedWindow = synAck.WindowSize;
+        Console.WriteLine($"Received SYN-ACK. Agreed Window size {agreedWindow}");
+
+        Packet ack = new Packet
+        {
+            Type = PacketType.ACK,
+            SequenceNumber = 0,
+            WindowSize = agreedWindow
+        };
+
+        udpClient.Send(ack.Serialize(),serverEndPoint);
+        Console.WriteLine("Sent ACK. Handshake complete!");
 
     }
 }
